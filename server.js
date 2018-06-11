402cfd2cc2b6f1e33de45cc286c8cee5cf965fc2
Kennan Smith <kennan.smith1@gmail.com>
2018-06-11 11:34:51 -0600

    added passport and express-session imports and configuration to server.js

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var session = require('express-session');
+
 
 var express = require('express');
 var app = express();
@@ -7,6 +10,7 @@ var PORT = process.env.PORT || 8080;
 // Requiring our models for syncing
 var db = require("./models");
  
+
 // Sets up the Express app to handle data parsing
 
 // parse application/x-www-form-urlencoded
@@ -16,6 +20,12 @@ app.use(bodyParser.json());
 
 // Static directory
 app.use(express.static("public"));
+
+
+// Passport configuration
+app.use(session({ secret: "secrets save", resave: true, saveUninitialized: true})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
 
 // Routes
 // =============================================================
