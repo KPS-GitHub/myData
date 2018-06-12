58b79b2c4b4acb897d5ac39dc7a4efbdca3133b9
Kennan Smith <kennan.smith1@gmail.com>
2018-06-12 13:33:57 -0600

    removed public folder, transferred html files over to hbs files stored in app/views, same for routes

--- app/routes/html-routes.js
+++ app/routes/html-routes.js
@@ -2,7 +2,17 @@ var path = require("path");
 
 
 module.exports = function(app) {
-    app.get("/", function(req, res) {
-        res.send('Welcome to the myData / page');
+
+    app.get("/", function(req,res) {
+        res.sendFile(path.join(__dirname, "../views/home.hbs"));
     });
+
+    app.get("/calories", function(req,res) {
+        res.sendFile(path.join(__dirname, "../views/calories.hbs"));
+    });
+
+    app.get("/spending", function(req,res) {
+        res.sendFile(path.join(__dirname, "../views/spending.hbs"));
+    });
+
 }
