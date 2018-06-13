a5c401484ca4957bd1dd8e570f52e9dce1090d2b
Kennan Smith <kennan.smith1@gmail.com>
2018-06-13 14:32:03 -0600

    finally fixed path issue - hopefully heroku will work now

--- app/config/middleware/isAuthenticated.js
+++ app/config/middleware/isAuthenticated.js
@@ -1,3 +1,4 @@
+var path = require("path");
 // This is middleware for restrictng routes a user is not allowed to visit if not logged in
 module.exports = function(req, res, next) {
     // If the user is logged in, continue with the request to the restricted route
@@ -6,6 +7,6 @@ module.exports = function(req, res, next) {
     }
   
     // If the user isnt logged in, redirect them to the signup page
-    return res.sendFile("../../../public/signup.html");
+    return res.sendFile(path.join(__dirname, "../../../public/signup.html"));
 };
   
