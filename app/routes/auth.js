dbc28aad116bc22c005b4efd56f224cff1b8a844
Kennan Smith <kennan.smith1@gmail.com>
2018-06-13 12:29:13 -0600

    changed middleware to redirect to signup page rather than signin, signup page will include a link to the signin page

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -20,7 +20,7 @@ module.exports = function(app, passport) {
     app.post('/signin', passport.authenticate('local-signin', {
             successRedirect: '../../public/home.html',
 
-            failureRedirect: '/signin'
+            failureRedirect: '/signup'
         }
 
     ));
