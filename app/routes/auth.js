3d6fa275a87ba2314a6c9f8d8bc8b8d693f81152
Kennan Smith <kennan.smith1@gmail.com>
2018-06-13 12:26:12 -0600

    middleware is working - auth.js was updated to redirect to the home.html file in the public folder - not sure if it will work as I replaced the hbs route to dashboard.hbs to a file path that leads to public/home.html

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -7,18 +7,18 @@ module.exports = function(app, passport) {
     app.get('/signin', authController.signin);
 
     app.post('/signup', passport.authenticate('local-signup', {
-            successRedirect: '/dashboard',
+            successRedirect: '../../public/home.html',
     
             failureRedirect: '/signup'
         }
     ));
 
-    app.get('/dashboard', isLoggedIn, authController.dashboard);
+    // app.get('/dashboard', isLoggedIn, authController.dashboard);
 
     app.get('/logout',authController.logout);
 
     app.post('/signin', passport.authenticate('local-signin', {
-            successRedirect: '/dashboard',
+            successRedirect: '../../public/home.html',
 
             failureRedirect: '/signin'
         }
