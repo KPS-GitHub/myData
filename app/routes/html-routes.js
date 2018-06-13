ec1ca94038646cfac2472d31e820af5ca838bfee
Kennan Smith <kennan.smith1@gmail.com>
2018-06-13 12:15:24 -0600

    added login authentication express middleware - to be tested after changes are pushed to heroku

--- app/routes/html-routes.js
+++ app/routes/html-routes.js
@@ -1,28 +1,23 @@
 var path = require("path");
+
 var db = require("../models");
+
+var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
 
-    function loggedIn(req,res) {
+    app.get("/", isAuthenticated, function(req,res) {
+        res.sendFile(path.join(__dirname, "../../public/home.html"));
+    });
 
-        app.get("/", function(req,res) {
-            if (req.user) {
-                res.sendFile(path.join(__dirname, "../../public/home.html"));
-            } else {
-                res.render('signin');
-            }
-        });
-    }
- 
 
-        // app.get("/calories", function(req,res) {
-        //     res.sendFile(path.join(__dirname, "../../public/calories.html"));
-        // });
+    // app.get("/calories", function(req,res) {
+    //     res.sendFile(path.join(__dirname, "../../public/calories.html"));
+    // });
 
-        app.get("/spending", function(req,res) {
-            res.sendFile(path.join(__dirname, "../../public/spending.html"));
-        });
+    app.get("/spending", isAuthenticated, function(req,res) {
+        res.sendFile(path.join(__dirname, "../../public/spending.html"));           
+    });
 
     
-
 }
