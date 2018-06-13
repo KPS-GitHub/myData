a057370928309873ca9867704ad2c86e30bc6dbb
Kennan Smith <kennan.smith1@gmail.com>
2018-06-12 20:14:32 -0600

    filled out spending.js in public folder so that it can post a new purchase to the database

--- public/js/spending.js
+++ public/js/spending.js
@@ -1,5 +1,39 @@
 $(document).ready(function() {
 
+// getting jquery references
+var spendingAmount = $("#dollars");
+var spendingCategory = $("#category");
+var spendingForm = $("#spending");
+
+// event listener for when form is submitted
+$(spendingForm).on("submit", handleFormSubmit);
+
+// A function for handling what happens when the form to create a new post is submitted
+function handleFormSubmit(event) {
+    event.preventDefault();
+    // Wont submit the purchase if we are missing an amount or category
+    if (!spendingAmount.val().trim() || !spendingCategory.val().trim()) {
+      return;
+    }
+    // Constructing a newPurchase object to hand to the database
+    var newPurchase = {
+      amount: spendingAmount
+        .val()
+        .trim(),
+      category: spendingCategory
+        .val()
+        .trim()
+    };
+
+
+    submitPurchase(newPurchase);
+    
+}
+
+// Submits a new purchase
+function submitPost(post) {
+    $.post("/api/posts", post);
+}
 
 
 }); //end of docready
