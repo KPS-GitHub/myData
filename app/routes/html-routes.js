dc6affbd881107e84c474fdf7f72067fac1bf18b
Kennan Smith <kennan.smith1@gmail.com>
2018-06-14 16:22:48 -0600

    added styling to signup/signin pages and gave them links to each other - project now only needs to incorporate css animations and all requirements will be met

--- app/routes/html-routes.js
+++ app/routes/html-routes.js
@@ -7,7 +7,7 @@ var isAuthenticated = require("../config/middleware/isAuthenticated");
 module.exports = function(app, passport) {
 
     app.get("/", isAuthenticated, function(req,res) {
-        res.sendFile(path.join(__dirname, "../../public/home.html"));
+        res.sendFile(path.join(__dirname, "../../public/spending.html"));
     });
 
 
